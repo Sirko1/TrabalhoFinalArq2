d80909d3802cbc64e442fd26a1dcac3bc53126d4
matheusDeAlmeida <mat.sts.amd@gmail.com>
2015-06-27 11:09:52 -0300

    Update mandelbrotParalelo.c
    Inicio das paralelizações; moveram-se para o registrador mm1 63, 127 e 255 para realizar a multiplicação simd. O tamanho de color[] foi ambliado de 3 para 8  com o intuito de movê-lo como qword e realizar multiplicações simultâneas de 16 bits.

--- mandelbrotParalelo.c
+++ mandelbrotParalelo.c
@@ -1,21 +1,12 @@
-/* 
-Modified from: http://rosettacode.org/wiki/Mandelbrot_set#PPM_non_interactive
-c program:
---------------------------------
-1. draws Mandelbrot set for Fc(z)=z*z +c
-using Mandelbrot algorithm ( boolean escape time )
--------------------------------         
-2. technique of creating ppm file is  based on the code of Claudio Rocchini
-http://en.wikipedia.org/wiki/Image:Color_complex_plot.jpg
-create 24 bit color graphic file ,  portable pixmap file = PPM 
-see http://en.wikipedia.org/wiki/Portable_pixmap
-to see the file use external application ( graphic viewer)
-*/
 #include <stdio.h>
 #include <math.h>
 
 int main()
 {
+		//variaveis auxiliares para uso de instrucoes simd
+		static char multiplicador[]= {0,63,0,127,0,255,0,0};
+
+
         /* screen ( integer) coordinate */
         int iX,iY;
         const int iXmax = 16384; 
@@ -34,7 +25,7 @@ int main()
         const int MaxColorComponentValue=255; 
         FILE * fp;
         char *filename="mandelbrot.ppm";
-        static unsigned char color[3];
+        static unsigned char color[8]; //color[3] pra color[8]
         /* Z=Zx+Zy*i  ;   Z0 = 0 */
         double Zx, Zy;
         double Zx2, Zy2; /* Zx2=Zx*Zx;  Zy2=Zy*Zy  */
@@ -49,19 +40,27 @@ int main()
         /*write ASCII header to the file*/
         fprintf(fp,"P6\n %d\n %d\n %d\n",iXmax,iYmax,MaxColorComponentValue);
         /* compute and write image data bytes to the file*/
+
+		 __asm{ //fora dos loops por questao de eficiencia
+			mov edx, offset multiplicador[0]
+			movq mm1, ptr qword[edx]   //mm1=     63   :  127     :   255
+		 }
+
+
+
         for(iY=0;iY<iYmax;iY++)
         {
              Cy=CyMin + iY*PixelHeight;
+			 
              if (fabs(Cy)< PixelHeight/2) Cy=0.0; /* Main antenna */
              for(iX=0;iX<iXmax;iX++)
              {         
                         Cx=CxMin + iX*PixelWidth;
                         /* initial value of orbit = critical point Z= 0 */
                         Zx=0.0;
-                        Zy=0.0;
-                        Zx2=Zx*Zx;
-                        Zy2=Zy*Zy;
-                        /* */
+                        Zy=0.0;//parte float, nao tem como paralelizar
+						Zx2=Zx*Zx;
+						Zy2=Zy*Zy;
                         for (Iteration=0;Iteration<IterationMax && ((Zx2+Zy2)<ER2);Iteration++)
                         {
                             Zy=2*Zx*Zy + Cy;
@@ -72,15 +71,29 @@ int main()
                         /* compute  pixel color (24 bit = 3 bytes) */
                         if (Iteration==IterationMax)
                         { /*  interior of Mandelbrot set = black */
-                           color[0]=0;
-                           color[1]=0;
-                           color[2]=0;                           
-                        }
+						__asm{
+								//color[0]=0; //color[0]:color[1] --aqui tratamos os operandos de memoria
+								//color[1]=0; //color[2]:color[3] --como ax
+								//color[2]=0; //color[4]:color[5] --ax = ah:al
+								mov edx, offset color[0]
+								pxor mm0, mm0 //mm0=0
+								movq ptr qword[edx], mm0
+							}
+						}
                         else 
                         { /* exterior of Mandelbrot set = white */
-                             color[0]=((IterationMax-Iteration) % 8) *  63;  /* Red */
-                             color[1]=((IterationMax-Iteration) % 4) * 127;  /* Green */ 
-                             color[2]=((IterationMax-Iteration) % 2) * 255;  /* Blue */
+							__asm{
+								//color[0]=((IterationMax-Iteration) % 8) *  63;  /* Red */
+								//color[1]=((IterationMax-Iteration) % 4) * 127;  /* Green */ 
+								//color[2]=((IterationMax-Iteration) % 2) * 255;  /* Blue */
+								color[1]=((IterationMax-Iteration) % 8);  /* Red */
+								color[3]=((IterationMax-Iteration) % 4);  /* Green */ 
+								color[5]=((IterationMax-Iteration) % 2);  /* Blue */
+								
+								mov edx, offset color[0]
+								movq mm2, ptr qword[edx]   //mm0= color[0] : color[1] : color[2]
+																
+							 }
                         };
                         /*write color to the file*/
                         fwrite(color,1,3,fp);
