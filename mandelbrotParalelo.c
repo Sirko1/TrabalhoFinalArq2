8cf1086cb4e1f62e540059cf13c9a804d54836f2
matheusDeAlmeida <mat.sts.amd@gmail.com>
2015-06-27 18:13:22 -0300

    Update mandelbrotParalelo.c

--- mandelbrotParalelo.c
+++ mandelbrotParalelo.c
@@ -4,8 +4,9 @@
 int main()
 {
 		//variaveis auxiliares para uso de instrucoes simd
-		static char multiplicador[]= {0,63,0,127,0,255,0,0};
+		static char multiplicador[]= {63,0,127,0,255,0,0,0};
 
+		int cont=0, cont2=0;
 
         /* screen ( integer) coordinate */
         int iX,iY;
@@ -26,6 +27,7 @@ int main()
         FILE * fp;
         char *filename="mandelbrot.ppm";
         static unsigned char color[8]; //color[3] pra color[8]
+		static unsigned char colorArquivo[3]; //pra escrever no arquivo
         /* Z=Zx+Zy*i  ;   Z0 = 0 */
         double Zx, Zy;
         double Zx2, Zy2; /* Zx2=Zx*Zx;  Zy2=Zy*Zy  */
@@ -43,22 +45,25 @@ int main()
 
 		 __asm{ //fora dos loops por questao de eficiencia
 			mov edx, offset multiplicador[0]
-			movq mm1, ptr qword[edx]   //mm1=     63   :  127     :   255
+			movq mm2, qword ptr [edx]   //mm1=     63   :  127     :   255
 		 }
 
 
 
         for(iY=0;iY<iYmax;iY++)
         {
-             Cy=CyMin + iY*PixelHeight;
-			 
+			 cont++;
+			 cont2++;
+			 if(cont==1000)
+				 scanf("%d", &cont);
+
+			 Cy=CyMin + iY*PixelHeight;
              if (fabs(Cy)< PixelHeight/2) Cy=0.0; /* Main antenna */
              for(iX=0;iX<iXmax;iX++)
              {         
                         Cx=CxMin + iX*PixelWidth;
-                        /* initial value of orbit = critical point Z= 0 */
                         Zx=0.0;
-                        Zy=0.0;//parte float, nao tem como paralelizar
+                        Zy=0.0;
 						Zx2=Zx*Zx;
 						Zy2=Zy*Zy;
                         for (Iteration=0;Iteration<IterationMax && ((Zx2+Zy2)<ER2);Iteration++)
@@ -72,31 +77,42 @@ int main()
                         if (Iteration==IterationMax)
                         { /*  interior of Mandelbrot set = black */
 						__asm{
-								//color[0]=0; //color[0]:color[1] --aqui tratamos os operandos de memoria
-								//color[1]=0; //color[2]:color[3] --como ax
-								//color[2]=0; //color[4]:color[5] --ax = ah:al
 								mov edx, offset color[0]
 								pxor mm0, mm0 //mm0=0
-								movq ptr qword[edx], mm0
+								movq qword ptr [edx], mm0
 							}
 						}
                         else 
                         { /* exterior of Mandelbrot set = white */
+
+
+							color[0]=((IterationMax-Iteration) % 8);  /* Red */
+							color[2]=((IterationMax-Iteration) % 4);  /* Green */ 
+							color[4]=((IterationMax-Iteration) % 2);  /* Blue */
+
 							__asm{
-								//color[0]=((IterationMax-Iteration) % 8) *  63;  /* Red */
-								//color[1]=((IterationMax-Iteration) % 4) * 127;  /* Green */ 
-								//color[2]=((IterationMax-Iteration) % 2) * 255;  /* Blue */
-								color[1]=((IterationMax-Iteration) % 8);  /* Red */
-								color[3]=((IterationMax-Iteration) % 4);  /* Green */ 
-								color[5]=((IterationMax-Iteration) % 2);  /* Blue */
+
 								
 								mov edx, offset color[0]
-								movq mm2, ptr qword[edx]   //mm0= color[0] : color[1] : color[2]
-																
-							 }
-                        };
-                        /*write color to the file*/
-                        fwrite(color,1,3,fp);
+								movq mm1, qword ptr [edx]   //mm1= color[0] : color[1] : color[2]
+								
+								pmullw mm1, mm2 // mm1*=mm2 (multiplicacao com sinal, mas nao importa)
+								
+								movq [edx], mm1						
+							 };
+                        };						
+
+						//colorArquivo serah escrito no arquivo                        
+						__asm{
+							mov al, color[0]
+							mov colorArquivo[0], al
+							mov al, color[2]
+							mov colorArquivo[1], al
+							mov al, color[4]
+							mov colorArquivo[2], al
+						};
+						
+                        fwrite(colorArquivo,1,3,fp);
                 }
         }
         fclose(fp);
