069040cd71f290b976d0ef95210844a90c9cb85f
matheusDeAlmeida <mat.sts.amd@gmail.com>
2015-07-08 17:06:29 -0300

    Update make.bat

--- make.bat
+++ make.bat
@@ -1,4 +1,4 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+REM call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 
 cl mandelbrotOriginal.c
 cl mandelbrotMMX.c
